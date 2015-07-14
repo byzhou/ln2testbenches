3be98203baa9668e39a8c36d91589a229a751739
bob-icsg <Boyou.Zhou@analog.com>
2015-07-14 09:40:34 -0400

    readme added

---
+++ README.md
@@ -0,0 +1,4 @@
+Protected under MIT license
+
+The code is to create lns to cadence project in order to gather all the sv code in the current
+directory.
--- verilog_links.py
+++ verilog_links.py
@@ -1,31 +1,12 @@
 #!/usr/bin/env python
 
-#
-# Usage:
-#
-# verilog_links.py [directory] [viewname] 
-# 
-# directory (optional):
-#    the relative directory (such as the library root) you want for the root
-#    defaults to current directory
-#
-# viewname (optional):
-#    the naname of the cellview used in Cadence that is to be linked
-#    defaults to 'verilogams'
-#
-
-
-
 import os, sys
 from os.path import normpath, join
 
 if len(sys.argv) > 1:
     libroot = sys.argv[1]
 else:
-    libroot = '/home/bzhou3/view_projects/adp107x/active/adp107x_ar1_rnm' #change the searching path
-
-#cellname =  os.path.basename( os.path.dirname (os.path.dirname ( libroot ) ) ) 
-#os.system ( 'echo ' + '"' + cellname + '"') 
+    libroot = '/path/to/cadence/project' #change the searching path
 
 if len(sys.argv) > 2:
     viewname1 = sys.argv[2]
@@ -34,6 +15,8 @@ if len(sys.argv) > 2:
 else:
     viewname1 = 'verilogams'
     viewname2 = 'systemVerilog'
+
+# ln 2 both verilogams and systemverilog
 
 base = os.getcwd()
 rootpath = normpath(join(base, libroot))
@@ -44,7 +27,6 @@ for root, dirs, files in os.walk(rootpath):
       and 'verilog.v' not in files:
         print root
         cellname = os.path.basename(os.path.dirname(root))
-        #os.chdir(root)
         if 'verilog.vams' in files: 
             os.system('ln -svf '+ root + '/verilog.vams ' + cellname + '.vams')
         elif 'verilog.sv' in files: 
